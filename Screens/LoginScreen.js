09ff9d5beb8615ce6bd29fdfb72bd9fccfe932d1
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-12-21 23:33:19 -0600

    add Login screen

---
+++ Screens/LoginScreen.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, Input, Image, Text } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { KeyboardAvoidingView } from "react-native";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {};
+
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+
+      <Image
+        source={{
+          uri: "https://img.icons8.com/color/480/signal-app.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+
+      <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+      <Button containerStyle={styles.button} type="outline" title="Registar" />
+      <View style={{ height: 100 }} />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDsTAqbWYxKE92GfhPATQLokVSWCD1hPog",
+  authDomain: "signal-clone-42b90.firebaseapp.com",
+  projectId: "signal-clone-42b90",
+  storageBucket: "signal-clone-42b90.appspot.com",
+  messagingSenderId: "598244891101",
+  appId: "1:598244891101:web:53ba1c5210f3868922be34",
+};
